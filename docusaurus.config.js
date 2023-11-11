f58e7241617530470e3fbae8145dec597e74ce96
bobytudu <34004403+bobytudu@users.noreply.github.com>
2023-11-11 23:38:43 +0530

    modified configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,17 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://bobytudu.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/react-componentry-docs/",
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "bobytudu", // Usually your GitHub org/user name.
+  projectName: "react-componentry-docs", // Usually your repo name.
+  deploymentBranch: "gh-pages", // The branch of your docs repo that you are publishing to GitHub pages
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -44,14 +46,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/bobytudu/react-componentry/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/bobytudu/react-componentry/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -127,7 +129,7 @@ const config = {
               // },
               {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/bobytudu/react-componentry",
               },
             ],
           },
